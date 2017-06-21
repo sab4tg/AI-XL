299fc26bc959d968fde8ff84f326f47b3403eb5f
christianokada <christian.okada@gmail.com>
2017-06-21 10:33:58 -0400

    editz

--- aixl.py
+++ aixl.py
@@ -1,22 +1,33 @@
+from __future__ import unicode_literals
 import openpyxl
 
 # wb = Workbook()
-wb = openpyxl.load_workbook('contest-test.xlsx')
+wb = openpyxl.load_workbook('contest-train.xlsx')
 
 # dict = {'ACN/AFS Curriculum': 'CDP'}
 
 #loop through rows and populate category(AA) value
 ws = wb.active
 
-for row in ws.iter_rows('AB{}:AB{}'.format(ws.min_row,ws.max_row)):
-    value = ws['AB'+str(c)]
-    print value
+AB = []
+Q = []
 
-for row in ws.iter_rows('AA{}:AA{}'.format(ws.min_row,ws.max_row)):
+for row in ws.iter_rows('AB{}:AB{}'.format(ws.min_row,ws.max_row)):
+    for cell in row:
+        AB.append(cell.value.encode("ascii", "ignore"))
+
+for row in ws.iter_rows('Q{}:Q{}'.format(ws.min_row,ws.max_row)):
+    for cell in row:
+        Q.append(cell.value.encode("ascii", "ignore"))
+c = 2
+for row in ws.iter_rows('AA{}:AA{}'.format(2,ws.max_row)):
     for cell in row:
         # cell.value = 'ACN/AFS Curriculum'
-        print cell.value
+        print str(c)+":     AB:"+AB[c]+"\tQ:"+Q[c]+"\tAA:"+str(cell.value)
+        c += 1
+
+        if c > 2701:
+            exit()
 
 # Save the file
 # wb.save("contest-test.xlsx")
-
